76c92da1320a456bd31e5283fec3a75cb1afe131
vaibhav <vaibhavsahni2001@gmail.com>
2023-06-16 18:33:50 +0530

    added box

--- pages/index.js
+++ pages/index.js
@@ -16,12 +16,12 @@ export default function Home() {
     }  
   })
   return (
-    <div className='w-full h-screen bg-cover' style={{backgroundImage:"url('/login.jpg')"}}>
-      <div className='p-10 flex flex-col items-center sm:items-end'>
+    <div className='w-full h-screen bg-cover flex justify-center items-center' style={{backgroundImage:"url('/login.jpg')"}}>
+      <div className='p-10 flex flex-col items-center sm:items-end bg-[#282c34] rounded-md'>
         <div className='sm:w-96 w-auto'>
-          <h1 className='text-white text-5xl mb-5'>Welcome to Todo!</h1>
-          <p className='text-blue-300 text-lg mb-5'> "time management application that combines the pomodoro technique and task list."</p>
-          <Link className='bg-gradient-to-r from-violet-500 to-fuchsia-500 w-full block text-center rounded-full text-white font-semibold text-lg' href={'/api/auth/login'}>Login</Link>
+          <h1 className='text-blue-100 text-5xl mb-5'>Welcome to Todo!</h1>
+          <p className='text-blue-100 text-lg mb-5'> "time management application that combines the pomodoro technique and task list."</p>
+          <Link className='bg-gradient-to-r from-sky-500 to-indigo-500 w-full block text-center rounded-full text-white font-semibold text-lg' href={'/api/auth/login'}>Login</Link>
         </div>
       </div>
     </div>
